29896e21a9fbfe44927bc176d74100bbd017b643
Tor Grønås <tor.gronas@hotmail.com>
2022-05-21 16:16:54 +0200

    Add video

--- app.js
+++ app.js
@@ -7,10 +7,13 @@ app.set("view engine", "ejs");
 
 app.use(express.static("public"));
 
+app.use("/dist", express.static("/node_modules/video.js/dist"));
+app.use("/css", express.static("./node_modules/bootstrap/dist/css"));
+app.use("/js", express.static("./node_modules/bootstrap/dist/js"));
+
 app.get("/", (req, res) => {
     res.render("home");
 });
-
 
 
 
