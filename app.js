1251f71597db9f63facbc6f00b4cef8f83c59f96
Tor Grønås <tor.gronas@hotmail.com>
2022-06-01 17:35:00 +0200

    kommentar

--- app.js
+++ app.js
@@ -7,9 +7,11 @@ app.set("view engine", "ejs");
 
 app.use(express.static("public"));
 
-app.use("/dist", express.static("/node_modules/video.js/dist"));
+app.use("/videojs", express.static("./node_modules/video.js/dist"));
 app.use("/css", express.static("./node_modules/bootstrap/dist/css"));
 app.use("/js", express.static("./node_modules/bootstrap/dist/js"));
+app.use("/jquery", express.static("./node_modules/jquery/dist"));
+app.use("/popperjs", express.static("./node_modules/@popperjs/core/dist/umd"));
 
 app.get("/", (req, res) => {
     res.render("home", {page_name: "home"});
